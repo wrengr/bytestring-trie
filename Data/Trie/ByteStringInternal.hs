b3925cb06fec9a28f5d2b416c7c213302ef43eb9
wren ng thornton <wren@community.haskell.org>
2008-12-24 00:37:48 -0500

    Data.Trie.ByteStringInternal: made indexOfDifference take a Word stride for as long as it can, before reverting to a Word8 stride.
    darcs-hash:f84e0d93b39ae6caaf052e72927a474ce1460fa8

--- Data/Trie/ByteStringInternal.hs
+++ Data/Trie/ByteStringInternal.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS_GHC -Wall -fwarn-tabs #-}
 
 ----------------------------------------------------------------
---                                                  ~ 2008.12.19
+--                                                  ~ 2008.12.24
 -- |
 -- Module      :  Data.Trie.ByteStringInternal
 -- Copyright   :  Copyright (c) 2008--2009 wren ng thornton
@@ -16,7 +16,7 @@
 
 module Data.Trie.ByteStringInternal
     ( ByteString, ByteStringElem
-    , wordHead
+    , unsafeWordHead
     , splitMaximalPrefix
     ) where
 
@@ -36,41 +36,48 @@ import Data.Bits
 
 type ByteStringElem = Word8 -- Associated type of ByteString
 
+-- | The size of 'Word' in bytes.
+sizeOfWord :: Int
+sizeOfWord  = sizeOf (undefined :: Word)
 
 ----------------------------------------------------------------
 -- | Return the first natural 'Word' worth of string, padding by
--- zeros as necessary. Number of elements per word, and position
--- of elements within the word varies by architecture.
-wordHead :: ByteString -> Word
-wordHead (PS s o l) = inlinePerformIO $
-                          withForeignPtr s $ \p ->
-                              liftM (maskBytes l .&.)
-                                  (peek (p `plusPtr` o :: Ptr Word))
+-- zeros as necessary. The position of elements within the word
+-- varies by architecture, hence this function is quasi-unsafe to
+-- use for trieing on the bit-vector representation. A safer version
+-- may be forthcoming, though trieing on multiple bytes at once
+-- appears impractical at the moment.
+unsafeWordHead :: ByteString -> Word
+unsafeWordHead (PS s o l) = inlinePerformIO $
+                                withForeignPtr s $ \p ->
+                                    liftM (maskInitialBytes l .&.)
+                                        (peek (p `plusPtr` o :: Ptr Word))
 
--- The 0x are automatically truncated when too large, don't need
--- to worry about 'max'ing with sizeOf Word.
-maskBytes :: Int -> Word
-maskBytes i
-    | isLittleEndian = case 0 `max` i of
-        0 -> 0x0000000000000000
-        1 -> 0x00000000000000FF
-        2 -> 0x000000000000FFFF
-        3 -> 0x0000000000FFFFFF
-        4 -> 0x00000000FFFFFFFF
-        5 -> 0x000000FFFFFFFFFF
-        6 -> 0x0000FFFFFFFFFFFF
-        7 -> 0x00FFFFFFFFFFFFFF
-        _ -> 0xFFFFFFFFFFFFFFFF
-    | otherwise = case 0 `max` i of
-        0 -> 0x0000000000000000
-        1 -> 0xFF00000000000000
-        2 -> 0xFFFF000000000000
-        3 -> 0xFFFFFF0000000000
-        4 -> 0xFFFFFFFF00000000
-        5 -> 0xFFFFFFFFFF000000
-        6 -> 0xFFFFFFFFFFFF0000
-        7 -> 0xFFFFFFFFFFFFFF00
-        _ -> 0xFFFFFFFFFFFFFFFF
+maskInitialBytes :: Int -> Word
+maskInitialBytes byteCount
+    | isLittleEndian = case effectiveByteCount of
+                       0 -> 0x0000000000000000
+                       1 -> 0x00000000000000FF
+                       2 -> 0x000000000000FFFF
+                       3 -> 0x0000000000FFFFFF
+                       4 -> 0x00000000FFFFFFFF
+                       5 -> 0x000000FFFFFFFFFF
+                       6 -> 0x0000FFFFFFFFFFFF
+                       7 -> 0x00FFFFFFFFFFFFFF
+                       _ -> 0xFFFFFFFFFFFFFFFF
+    | otherwise      = case effectiveByteCount of
+                       0 -> 0x0000000000000000
+                       1 -> 0xFF00000000000000
+                       2 -> 0xFFFF000000000000
+                       3 -> 0xFFFFFF0000000000
+                       4 -> 0xFFFFFFFF00000000
+                       5 -> 0xFFFFFFFFFF000000
+                       6 -> 0xFFFFFFFFFFFF0000
+                       7 -> 0xFFFFFFFFFFFFFF00
+                       _ -> 0xFFFFFFFFFFFFFFFF
+    where
+    effectiveByteCount = 0 `max` (byteCount `min` sizeOfWord)
+
 
 -- TODO: How to get this to execute statically?...
 -- BUG? is 'alloca' safe in 'inlinePerformIO' (used in 'wordHead')?
@@ -124,19 +131,38 @@ newPS s o l = if l <= 0 then S.empty else PS s o l
 (!$)  = ($!) -- fix associativity bug
 
 
--- Calculates the first index where values differ.
--- BUG: There has to be a smarter algorithm for this
+-- | Calculates the first index where values differ.
+--
+-- BUG: There has to be a smarter algorithm for this. In particular,
+-- it'd be nice if there was a function like C's @memcmp@, but which
+-- returns the index rather than the 'Ordering'. If it were agnostic
+-- ot architecture, that'd be even nicer.
+--
+-- TODO: rather than revert to @goByte@, we should use xor- and
+-- mask-munging so @goWord@ can reuse the information it already
+-- has to discover the byte of difference. The trick is making that
+-- faster than the current version. And ensuring alignment.
 {-# INLINE indexOfDifference #-}
 indexOfDifference :: Ptr ByteStringElem -> Ptr ByteStringElem -> Int -> IO Int
-indexOfDifference p1 p2 limit = go 0
+indexOfDifference p1 p2 limit = goByte 0
     where
-    go n = if   n >= limit
-           then return limit
-           else do c1 <- peekElemOff p1 n
-                   c2 <- peekElemOff p2 n
-                   if c1 == c2
-                       then go $! n+1
-                       else return n
+    {- BUG: using this assumes ByteStrings are Word-aligned
+    goWord n = if   n + sizeOfWord >= limit
+               then goByte n
+               else do w1 <- peek (p1 `plusPtr` n :: Ptr Word)
+                       w2 <- peek (p2 `plusPtr` n :: Ptr Word)
+                       if w1 == w2
+                           then goWord $! n + sizeOfWord
+                           else goByte n
+    -}
+    
+    goByte n = if   n >= limit
+               then return limit
+               else do c1 <- peekElemOff p1 n
+                       c2 <- peekElemOff p2 n
+                       if c1 == c2
+                           then goByte $! n+1
+                           else return n
 
 ----------------------------------------------------------------
 ----------------------------------------------------------- fin.

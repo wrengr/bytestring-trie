ff5f82524d55b53eb5f5c886455c4aed57d8ade5
Michael J. Klein <lambdamichael@gmail.com>
2019-04-02 23:16:20 -0400

    remove original modules

--- test/test-text-trie.hs
+++ test/test-text-trie.hs
@@ -1,29 +1,16 @@
 
--- import qualified Data.Trie.ByteStringInternal.Test
 import qualified Data.Trie.TextInternal.Test
--- import qualified Data.Trie.Test
 import qualified Data.Trie.Text.Test
 import qualified FromListBench
 import qualified FromListBench.Text
--- import qualified TrieFile
 import qualified TrieFile.Text
 
 main :: IO ()
 main = do
-  -- putStrLn "Data.Trie.ByteStringInternal.Test"
-  -- Data.Trie.ByteStringInternal.Test.test
-  -- putStrLn "End: Data.Trie.ByteStringInternal.Test"
-  -- putStrLn ""
-
   putStrLn "Data.Trie.TextInternal.Test"
   Data.Trie.TextInternal.Test.test
   putStrLn "End: Data.Trie.TextInternal.Test"
   putStrLn ""
-
-  -- putStrLn "Data.Trie.Test"
-  -- Data.Trie.Test.test
-  -- putStrLn "End: Data.Trie.Test"
-  -- putStrLn ""
 
   putStrLn "Data.Trie.Text.Test"
   Data.Trie.Text.Test.testText
@@ -40,15 +27,8 @@ main = do
   putStrLn "End: FromListBench.Text"
   putStrLn ""
 
-  -- putStrLn "TrieFile"
-  -- TrieFile.test
-  -- putStrLn "End: TrieFile"
-  -- putStrLn ""
-
   putStrLn "TrieFile.Text"
   TrieFile.Text.testText
   putStrLn "End: TrieFile.Text"
   putStrLn ""
-
-
 

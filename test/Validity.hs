bf0187e10795dae51dc2f30e8eae8c6e52f433ed
wren romano <2998727+wrengr@users.noreply.github.com>
2021-12-12 16:40:22 -0800

    Test.Validity: added (still WIP)

---
+++ test/Validity.hs
@@ -0,0 +1,198 @@
+{-# OPTIONS_GHC -Wall -fwarn-tabs #-}
+
+----------------------------------------------------------------
+--                                                  ~ 2021.12.12
+-- |
+-- Module      :  test/Validity
+-- Copyright   :  2021 wren gayle romano, 2004--2021 libraries@haskell.org
+-- License     :  BSD3
+-- Maintainer  :  wren@cpan.org
+-- Stability   :  provisional
+-- Portability :  portable
+--
+-- Verify the invariants of 'Trie', a~la
+-- <https://github.com/haskell/containers/blob/master/containers-tests/tests/IntMapValidity.hs>
+----------------------------------------------------------------
+module Validity (valid) where
+
+-- BUG: requires exporting the constructors!
+import           Data.Trie.Internal           (Trie(..))
+import qualified Data.Trie.BitTwiddle   as BT (zero)
+import qualified Data.ByteString        as S  (null)
+import           Data.Bits                    ((.&.), popCount)
+import qualified Test.QuickCheck        as QC (Property, counterexample, (.&&.))
+
+----------------------------------------------------------------
+----------------------------------------------------------------
+
+data BrokenInvariant
+    = EmptyAfterReject  -- ^ Forbidden: @Arc _ Nothing Empty@.
+    | EmptyAfterBranch  -- ^ Forbidden: @Branch _ _ Empty _@ and @Branch _ _ _ Empty@.
+    | ArcAfterReject    -- ^ Forbidden: @Arc _ Nothing (Arc _ _ _)@.
+    | NonRootEpsilon    -- ^ Forbidden: @Arc S.empty (Just _) _@ except when at the root.
+    | EpsilonReject     -- ^ Forbidden: @Arc S.empty Nothing _@.
+    | MaskNotPowerOfTwo -- ^ 'Mask' must have exactly one bit set.
+    | PrefixNotMatched  -- ^ All keys after a branch must start with the 'Prefix'.
+    | MaskBitNotRespected
+        -- ^ The 'Mask' bit is 'BT.zero' for all keys in the left
+        -- subtrie, and not for any keys in the right subtrie.
+    deriving (Eq, Show)
+
+et :: Maybe e -> Maybe e -> Maybe e
+et m@(Just _) _ = m
+et Nothing    n = n
+
+-- This combines all the invariant checks into a single pass.
+-- However, the improved performance may not be worth the added
+-- code complexity, since now it's no longer trivial to see that
+-- this does indeed check the invariants.
+validate :: Trie a -> Maybe BrokenInvariant
+validate = start
+    where
+    start0 (Arc _ (Just _) t)           = start1 t -- Skip the non-null check at root.
+    start0 t                            = start1 t -- Otherwise same as usual.
+    -- This used to be the main recursion, but we inlined it into @checkArc@.
+    start1 Empty                        = Nothing
+    start1 (Arc k mv t)                 = checkArc k mv t
+    start1 b@(Branch{})                 = checkBranch b (const Nothing)
+    -- | Validate 'Arc' properties.
+    checkArc k mv       _   | S.null k  = Just (epsilonError mv)
+    checkArc _ (Just _) Empty           = Nothing
+    checkArc _ (Just _) (Arc k mv t)    = checkArc k mv t
+    checkArc _ Nothing  Empty           = Just EmptyAfterReject
+    checkArc _ Nothing  (Arc{})         = Just ArcAfterReject
+    checkArc _ _        b@(Branch{})    = checkBranch b (const Nothing)
+    -- | Validate 'Branch' properties.
+    -- The code complexity is to avoid O(n^2) traversal of branch
+    -- collections, which would happen if we checked @all _ (keyHeads _)@
+    -- in the more stratightforward manner.
+    checkBranch Empty            _      = Just EmptyAfterBranch
+    checkBranch (Arc k mv t)     pred
+        | S.null k  = Just (epsilonError mv) -- redundant with @checkArc@, but has to be before we take the 'S.head'
+        | otherwise = pred (S.unsafeHead k) `et` checkArc k mv t
+    checkBranch (Branch p m l r) pred
+        | popCount m /= 1               = Just MaskNotPowerOfTwo
+        | otherwise =
+            checkBranch l (predKeyHeadsLeft  p m pred) `et`
+            checkBranch r (predKeyHeadsRight p m pred)
+
+    predKeyHeadsLeft p m pred = \x ->
+        if p /= p .&. x      then Just PrefixNotMatched    else
+        if not (BT.zero x m) then Just MaskBitNotRespected else
+        pred x
+
+    predKeyHeadsRight p m pred = \x ->
+        if p /= p .&. x then Just PrefixNotMatched    else
+        if BT.zero x m  then Just MaskBitNotRespected else
+        pred x
+
+    epsilonError (Just _) = NonRootEpsilon
+    epsilonError Nothing  = EpsilonReject
+
+
+
+----------------------------------------------------------------
+----------------------------------------------------------------
+-- TODO: would be faster to combine all these invariant predicates
+-- into a single recursion; just the issue of specifying the failure
+-- mode, and keeping the logic clean\/obvious.
+--
+-- TODO: generalize the conjunction type so we can use smallcheck too.
+--
+-- | Returns true iff the internal structure of the 'Trie' is valid.
+valid :: Trie a -> QC.Property
+valid t = QC.counterexample "validShape"     (validShape     t)
+  QC..&&. QC.counterexample "validEpsilon"   (validEpsilon   t)
+  QC..&&. QC.counterexample "maskPowerOfTwo" (maskPowerOfTwo t)
+  QC..&&. QC.counterexample "commonPrefix"   (commonPrefix   t)
+  QC..&&. QC.counterexample "maskRespected"  (maskRespected  t)
+
+-- | Invariants:
+-- * 'Empty' may only occur at root and after an accept-'Arc'.
+-- * 'Branch' must not have 'Empty' as child.
+-- * Reject-'Arc' must have 'Branch' as child.
+validShape :: Trie a -> Bool
+validShape = go True -- allow empty at root.
+    where
+    go allowEmpty Empty     = allowEmpty
+    go _ (Arc _ Nothing  t) = isBranch t && go False t -- forbid empty after reject
+    go _ (Arc _ (Just _) t) =               go True  t -- allow  empty after accept
+    go _ (Branch _ _ l r)   = go False l && go False r -- forbid empty after branch
+
+    isBranch (Branch{}) = True
+    isBranch _          = False
+
+-- | Invariants:
+-- * Reject-'Arc' must not have null bytestring.
+-- * Accept-'Arc' may only have null bytestring at the root.
+validEpsilon :: Trie a -> Bool
+validEpsilon = start
+    where
+    start (Arc _ (Just _) t) = go t -- Skip the non-null check at root.
+    start t                  = go t -- Otherwise same as usual.
+    go    Empty              = True
+    go    (Arc k _ t)        = not (S.null k) && go t
+    go    (Branch _ _ l r)   = go l && go r
+
+-- | Invariant: The 'Mask' is a power of 2.
+-- TODO: validate that it is the largest bit position at which two
+-- keys of the trie differ.
+maskPowerOfTwo :: Trie a -> Bool
+maskPowerOfTwo = go
+    where
+    go Empty            = True
+    go (Arc _ _ t)      = go t
+    go (Branch _ m l r) = popCount (fromIntegral m) == 1 && go l && go r
+
+keyHeads :: Trie a -> [ByteStringElem]
+keyHeads = \t -> go t id []
+    where
+    go Empty            xs = xs
+    go (Arc k _ _)      xs = (S.head k :) . xs
+    go (Branch _ _ l r) xs = go l . go r . xs
+
+-- | Invariant: 'Prefix' is the common high-order bits that all
+-- elements share to the left of the 'Mask' bit.
+commonPrefix :: Trie a -> Bool
+commonPrefix = go
+    where
+    go Empty              = True
+    go (Arc _ _ t)        = go t
+    go b@(Branch p _ l r) = all (sharedPrefix p) (keyHeads b) && go l && go r
+        -- TODO: fix this O(n^2) traversal of the collection of branches.
+
+    sharedPrefix :: Prefix -> ByteStringElem -> Bool
+    sharedPrefix p a = p == p .&. a
+
+-- | Invariant: In 'Branch', left consists of the elements that
+-- don't have the 'Mask' bit set; right is all the elements that
+-- do.
+maskRespected :: Trie a -> Bool
+maskRespected = go
+    where
+    go Empty            = True
+    go (Arc _ _ t)      = go t
+    go (Branch _ m l r) = and
+        -- TODO: fix this O(n^2) traversal of the collection of branches.
+        [ all (\x ->      BT.zero x m)  (keyHeads l)
+        , all (\x -> not (BT.zero x m)) (keyHeads r)
+        , go l
+        , go r
+        ]
+
+-- TODO: see when this becomes faster than the O(n^2) algorithms
+-- above, in spite of the cost of constructing all the closures.
+-- TODO: might as well combine 'maskPowerOfTwo' in here too, even
+-- though it doesn't use 'keyHeads'
+commonPrefix_and_maskRespected :: Trie a -> Bool
+commonPrefix_and_maskRespected = start
+    where
+    start t = go t (const True)
+    go Empty            _    = True
+    go (Arc k _ t)      pred = pred (S.head k) && start t
+    go (Branch p m l r) pred
+        =  go l (\h -> pred h && (p == p .&. h) &&      BT.zero h m)
+        && go r (\h -> pred h && (p == p .&. h) && not (BT.zero h m))
+
+----------------------------------------------------------------
+----------------------------------------------------------- fin.

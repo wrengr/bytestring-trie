e5e4f4b6dbf35c4447941b70261e8175fc7474e4
wren romano <2998727+wrengr@users.noreply.github.com>
2022-03-06 13:31:32 -0800

    Bench.Regression: converting things to a more regression-like bench suite

--- dev/Bench/Regression.hs
+++ dev/Bench/Regression.hs
@@ -1,5 +1,5 @@
 {-# OPTIONS_GHC -Wall -fwarn-tabs #-}
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE CPP, BangPatterns #-}
 
 ----------------------------------------------------------------
 --                                                  ~ 2022.03.06
@@ -13,15 +13,18 @@
 --
 ----------------------------------------------------------------
 
-module Bench.Regression (main, readTrieFromFile) where
--- Exporting 'readTrieFromFile' for reuse in GHCi while debugging.
+module Bench.Regression (main) where
 
-import Bench.Foldable (realTrie_to_benchTrie, bgroup_Foldable)
-
+import           Shared.BaseCompat
 import qualified Data.Trie          as T
 -- import qualified Data.Trie.Internal as TI
 import qualified Data.ByteString    as S
 
+#if MIN_VERSION_base(4,7,0)
+-- [GHC 7.8.1]: "Data.Coerce" added to base.
+import           Data.Coerce        (Coercible, coerce)
+#endif
+import qualified Data.Foldable      as F
 import qualified Criterion.Main     as C
 import qualified System.IO          as Sys (withFile, IOMode(..), hIsEOF)
 import qualified System.Environment as Sys (getProgName, getArgs, withArgs)
@@ -55,11 +58,80 @@ main  = do
         Sys.exitFailure
       file:rest ->
         Sys.withArgs rest $ C.defaultMain
-          [ C.env (realTrie_to_benchTrie <$> readTrieFromFile file) $ \ t ->
-            bgroup_Foldable [t]
-            -- TODO: the other benchmark suites here.
+          [ C.env (readTrieFromFile file) $ \ t ->
+            bgroup_Foldable t
+            -- TODO: more benchmark suites here.
           ]
 
+----------------------------------------------------------------
+----------------------------------------------------------------
+type BenchmarkE     env = env -> C.Benchmark
+type BenchmarkableE env = env -> C.Benchmarkable
+
+bgroupE :: String -> [BenchmarkE env] -> BenchmarkE env
+bgroupE name bs = \e -> C.bgroup name (($ e) <$> bs)
+{-# INLINE bgroupE #-}
+
+benchE :: String -> BenchmarkableE env -> BenchmarkE env
+benchE name b = \e -> C.bench name (b e)
+{-# INLINE benchE #-}
+
+----------------------------------------------------------------
+#if MIN_VERSION_base(4,7,0)
+-- | From "Data.Functor.Utils", but isn't exported.  Is used heavily
+-- by the default implementations, since they use so many newtype
+-- wrappers.
+(#.) :: Coercible b c => (b -> c) -> (a -> b) -> (a -> c)
+(#.) _ = coerce
+{-# INLINE (#.) #-}
+
+-- My own variant, ignoring the second argument instead.
+-- TODO: would be good to rephrase this into @(#.)@ for eta\/inlining reasons.
+(.#) :: Coercible a b => (b -> c) -> (a -> b) -> (a -> c)
+(.#) f _ = coerce f
+{-# INLINE (.#) #-}
+#endif
+
+----------------------------------------------------------------
+-- Hopefully the @fmap Sum@ will be rewritten into 'coerce'.  Though
+-- we may want to do that explicitly...
+bgroup_Foldable :: BenchmarkE (T.Trie Int)
+bgroup_Foldable =
+  bgroupE "Foldable"
+  [ benchE "fold" $ C.nf
+-- FIXME: the version using @(. fmap Sum)@ is monstrously slower
+-- than the version using 'coerce'.  On the one hand that makes
+-- sense, but on the other hand it means we really should provide
+-- rewrite rules for our Functor instance.
+#if MIN_VERSION_base(4,7,0)
+      (F.fold .# fmap Sum)
+#else
+      (F.fold . fmap Sum)
+#endif
+  , benchE "foldMap"   $ C.nf (F.foldMap  Sum)
+#if MIN_VERSION_base(4,13,0)
+  , benchE "foldMap'"  $ C.nf (F.foldMap' Sum)
+#endif
+-- FIXME: this is monstrously slower than everything else.  I take
+-- it our old implementation is indeed faster than the default?  Or
+-- I guess it could be the fundamental inefficiency of 'foldr' vs
+-- the other three.
+  , benchE "foldr"     $ C.nf (F.foldr  (+) 0)
+#if MIN_VERSION_base(4,6,0)
+  , benchE "foldr'"    $ C.nf (F.foldr' (+) 0)
+#endif
+  , benchE "foldl"     $ C.nf (F.foldl  (+) 0)
+#if MIN_VERSION_base(4,6,0)
+  , benchE "foldl'"    $ C.nf (F.foldl' (+) 0)
+#endif
+{-
+  #if MIN_VERSION_base(4,8,0)
+  , benchE "length" $ C.nf F.length
+  , benchE "length" $ C.nf F.maximum -- Must first ensure non-empty!
+  , benchE "length" $ C.nf F.minimum -- Must first ensure non-empty!
+  #endif
+-}
+  ]
 
 ----------------------------------------------------------------
 ----------------------------------------------------------- fin.
